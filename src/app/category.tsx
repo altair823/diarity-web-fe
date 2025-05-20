6691da61a4e3bc4006d3b7161908f3b7021aa0ad
altair823 <kimth0734@gmail.com>
2025-05-21 06:25:04 +0900

    feat: 시간추적 기능 제거

--- src/app/category.tsx
+++ src/app/category.tsx
@@ -27,31 +27,20 @@ export function Category({ isSmallScreen }: CategoryProps) {
             <span className='ml-2 font-bold'>홈</span>
           </Link>
         </li>
-        <li
-          className={`${
-            isSmallScreen ? 'category-item-small' : 'category-item-large'
-          } ${currentPath === 'tasks' ? 'bg-purple-300' : 'hover:bg-gray-200'}`}
-          onClick={() => {
-            if (!useUser.getState().isLogin) {
-              window.location.href = '/login'
-            }
-          }}
-        >
-          <Link href='/tasks' className='flex items-center'>
-            <Image src={category_home} alt={'Task'} />
-            <span className='ml-2 font-bold'>시간 추적</span>
-          </Link>
-        </li>
-        {/* 추후에 인기 게시물 조회 기능이 개발되면 추가될 예정 */}
         {/*<li*/}
         {/*  className={`${*/}
         {/*    isSmallScreen ? 'category-item-small' : 'category-item-large'*/}
-        {/*  } ${currentCategory === 'popular' ? 'bg-purple-300' : 'hover:bg-gray-200'}`}*/}
+        {/*  } ${currentPath === 'tasks' ? 'bg-purple-300' : 'hover:bg-gray-200'}`}*/}
+        {/*  onClick={() => {*/}
+        {/*    if (!useUser.getState().isLogin) {*/}
+        {/*      window.location.href = '/login'*/}
+        {/*    }*/}
+        {/*  }}*/}
         {/*>*/}
-        {/*  <a href='#' className='flex items-center'>*/}
-        {/*    <Image src={category_popular} alt={'Popular'} />*/}
-        {/*    <span className='ml-2 font-bold'>인기</span>*/}
-        {/*  </a>*/}
+        {/*  <Link href='/tasks' className='flex items-center'>*/}
+        {/*    <Image src={category_home} alt={'Task'} />*/}
+        {/*    <span className='ml-2 font-bold'>시간 추적</span>*/}
+        {/*  </Link>*/}
         {/*</li>*/}
       </ul>
     </div>
