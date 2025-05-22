549dd87f93cb18f62ced86c6980f351c0b3d1c4f
altair823 <kimth0734@gmail.com>
2025-05-23 05:19:19 +0900

    feat: 최근 방문 페이지 정보에 버전 추가

--- src/store/recentVisitedStore.ts
+++ src/store/recentVisitedStore.ts
@@ -13,7 +13,9 @@ interface RecentVisitedPage {
 }
 
 interface RecentVisitedStore {
+  version: string
   recentVisitedPages: RecentVisitedPage[]
+
   addPage: (page: RecentVisitedPage) => void
   removePage: (url: string) => void
   clearPages: () => void
@@ -27,47 +29,67 @@ function htmlTaggedToText(html: string) {
 export const useRecentVisited = create(
   persist<RecentVisitedStore>(
     (set) => ({
+      version: process.env.NEXT_PUBLIC_RECENT_STORAGE_VERSION || '0.0.0',
       recentVisitedPages: [],
       addPage: (page: RecentVisitedPage) =>
         set((state) => {
-          page.titleSummary = htmlTaggedToText(page.titleSummary)
-          page.contentSummary = htmlTaggedToText(page.contentSummary)
-          const exists = state.recentVisitedPages.some(
-            (p) => p.url === page.url
-          )
+          // 버전 검사: 다른 경우 recentVisitedPages 초기화
+          let updatedPages = [...state.recentVisitedPages];
+          if (
+            !state.version ||
+            state.version !== (process.env.NEXT_PUBLIC_RECENT_STORAGE_VERSION || '0.0.0')
+          ) {
+            updatedPages = []; // 페이지 리스트 초기화
+          }
+
+          page.titleSummary = htmlTaggedToText(page.titleSummary);
+          page.contentSummary = htmlTaggedToText(page.contentSummary);
+          const exists = updatedPages.some((p) => p.url === page.url);
+
           if (page.titleSummary.length > MAX_SUMMARY_TITLE_LENGTH) {
-            page.titleSummary =
-              page.titleSummary.slice(0, MAX_SUMMARY_TITLE_LENGTH) + '...'
+            page.titleSummary = page.titleSummary.slice(0, MAX_SUMMARY_TITLE_LENGTH) + '...';
           }
           if (page.contentSummary.length > MAX_SUMMARY_CONTENT_LENGTH) {
-            page.contentSummary =
-              page.contentSummary.slice(0, MAX_SUMMARY_CONTENT_LENGTH) + '...'
+            page.contentSummary = page.contentSummary.slice(0, MAX_SUMMARY_CONTENT_LENGTH) + '...';
           }
+
           if (!exists) {
-            // 새로운 페이지 추가
-            const newPage = { ...page, timestamp: new Date().getTime() }
-            const updatedPages = [...state.recentVisitedPages, newPage]
-            updatedPages.sort((a, b) => b.timestamp - a.timestamp)
-            return {
-              recentVisitedPages: updatedPages.slice(0, MAX_RECENT_PAGES_COUNT),
-            }
+            const newPage = { ...page, timestamp: new Date().getTime() };
+            updatedPages = [...updatedPages, newPage];
           } else {
-            // 기존 페이지 타임스탬프 업데이트
-            const updatedPages = state.recentVisitedPages.map((p) =>
+            updatedPages = updatedPages.map((p) =>
               p.url === page.url ? { ...p, timestamp: new Date().getTime() } : p
-            )
-            updatedPages.sort((a, b) => b.timestamp - a.timestamp)
-            return {
-              recentVisitedPages: updatedPages.slice(0, MAX_RECENT_PAGES_COUNT),
-            }
+            );
           }
+
+          updatedPages.sort((a, b) => b.timestamp - a.timestamp);
+
+          return {
+            version: process.env.NEXT_PUBLIC_RECENT_STORAGE_VERSION || '0.0.0',
+            recentVisitedPages: updatedPages.slice(0, MAX_RECENT_PAGES_COUNT),
+          };
         }),
+
       removePage: (url: string) =>
-        set((state) => ({
-          recentVisitedPages: state.recentVisitedPages.filter(
-            (page) => page.url !== url
-          ),
-        })),
+        set((state) => {
+          let updatedPages = [...state.recentVisitedPages];
+
+          // 버전 검사: 다른 경우 recentVisitedPages 초기화
+          if (
+            !state.version ||
+            state.version !== (process.env.NEXT_PUBLIC_RECENT_STORAGE_VERSION || '0.0.0')
+          ) {
+            updatedPages = []; // 페이지 리스트 초기화
+          }
+
+          // 이후 로직 계속 실행 - 지정된 URL 제거
+          updatedPages = updatedPages.filter((page) => page.url !== url);
+
+          return {
+            version: process.env.NEXT_PUBLIC_RECENT_STORAGE_VERSION || '0.0.0',
+            recentVisitedPages: updatedPages,
+          };
+        }),
       clearPages: () => set({ recentVisitedPages: [] }),
     }),
     {
